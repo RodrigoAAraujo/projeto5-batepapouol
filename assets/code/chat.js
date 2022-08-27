7dccc2581e2701cb19423f2ca61239885a9e2fc1
Rodrigo Antunes Araujo <rodrigo.teth@gmail.com>
2022-08-27 16:59:12 -0300

    so falta responder à erros

--- assets/code/chat.js
+++ assets/code/chat.js
@@ -1,8 +1,16 @@
 const messagesBlock = document.querySelector("main ul")
-const participantsBlock = document.querySelector("aside ul")
+const participantsBlock = document.querySelector("aside .person")
 const sideBar = document.querySelector("aside")
+const fogger = document.querySelector(".fogger")
+const textInsert = document.querySelector("footer input")
+const textTowho = document.querySelector("footer h5")
+const button = document.querySelector("footer button")
+let counter = 0
+
+
 let me = localStorage.getItem("me");
 
+/*------Get Info----- */
 
 function getMessages(){
     const receive = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
@@ -11,6 +19,7 @@ function getMessages(){
 }
 function renderMessages(response){
     let info = response.data
+    messagesBlock.innerHTML = ""
     info.forEach(element => {
         if (element.type == "message"){
             messagesBlock.innerHTML += `
@@ -33,13 +42,11 @@ function renderMessages(response){
             `
         }
     });
-    console.log(document.querySelector("main ul"))
     scroll()
 }
 function errorMessages(error){
-
+    alert(error)
 }
-
 
 function getParticipants(){
     const receive = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants")
@@ -47,65 +54,201 @@ function getParticipants(){
     receive.catch(errorParticipants)
 }
 function renderParticipants(response){
-    participantsBlock.innerHTML = `
-        <li>
-            <ion-icon name="people"></ion-icon>
-            <p>Todos</p>
+    if(counter === 0){
+        participantsBlock.innerHTML = `
+            <li class="selected" onclick="selectPerson(this)">
+                <div>
+                    <ion-icon name="people"></ion-icon>
+                    <p>Todos</p>
+                </div>
+                <ion-icon name="checkmark"></ion-icon>
+            </li>
+            `
+        let info  = response.data
+        info.forEach((element) => {
+            participantsBlock.innerHTML += `
+                <li data-identifier="participant" onclick="selectPerson(this)" >
+                    <div>
+                        <ion-icon name="person-circle-outline"></ion-icon>
+                        <p>${element.name}</p>
+                    </div>
+                    <ion-icon name="checkmark"></ion-icon>
+                </li>`
+        })
+    }
+
+    if(counter > 0 ){
+        let person = document.querySelector("aside .person .selected p")
+
+        if (person === null || person.innerHTML == "Todos"){
+        participantsBlock.innerHTML = `
+        <li class="selected" onclick="selectPerson(this)">
+            <div>
+                <ion-icon name="people"></ion-icon>
+                <p>Todos</p>
+            </div>
+            <ion-icon name="checkmark"></ion-icon>
         </li>
-    `
-    let info  = response.data
-    console.log(info)
-    info.forEach((element) => {
-        participantsBlock.innerHTML += `
-        <li>
-            <ion-icon name="person-circle-outline"></ion-icon>
-            <p>${element.name}</p>
-        </li>`
-    })
+        `
+        let info  = response.data
+        info.forEach((element) => {
+            participantsBlock.innerHTML += `
+            <li data-identifier="participant" onclick="selectPerson(this)" >
+                <div>
+                    <ion-icon name="person-circle-outline"></ion-icon>
+                    <p>${element.name}</p>
+                </div>
+                <ion-icon name="checkmark"></ion-icon>
+            </li>`
+
+        })}else {
+            participantsBlock.innerHTML = `
+            <li onclick="selectPerson(this)">
+                <div>
+                    <ion-icon name="people"></ion-icon>
+                    <p>Todos</p>
+                </div>
+                <ion-icon name="checkmark"></ion-icon>
+            </li>
+            `
+            let info  = response.data
+            info.forEach((element) => {
+            if (person.innerHTML === element.name){
+            participantsBlock.innerHTML += `
+            <li data-identifier="participant" class="selected" onclick="selectPerson(this)" >
+                <div>
+                    <ion-icon name="person-circle-outline"></ion-icon>
+                    <p>${element.name}</p>
+                </div>
+                <ion-icon name="checkmark"></ion-icon>
+            </li>`
+            }else{
+                participantsBlock.innerHTML += `
+                <li data-identifier="participant" onclick="selectPerson(this)" >
+                    <div>
+                        <ion-icon name="person-circle-outline"></ion-icon>
+                        <p>${element.name}</p>
+                    </div>
+                    <ion-icon name="checkmark"></ion-icon>
+                </li>`
+            }})
+        }
+    }
+
+    counter = 1
+    sendToWho()
 }
 function errorParticipants(error){
-
+    alert(error)
 }
 
+/*----Keep On--- */
+
 function login(){
-    let sendName = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", {name: me})
+    let sendName = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", {name: me})
     sendName.then(ok)
     sendName.catch(errorLogin)
 }
 function ok(response){
-    console.log("ok")
 }
 function errorLogin(error){
+    alert(error)
+}
+
+function keepOn(){
+    setInterval(login, 4000)
+    setInterval(getMessages, 3000)
+    setInterval(getParticipants, 10000)
 
 }
+
+/*----Front End Funcionalities---- */
 
 function renderHeader(){
     const name = document.querySelector("header div h4")
     name.innerHTML = me
 }
 function showSidebar(){
-    sideBar.classList.remove("hidden")
+    sideBar.classList.remove("noSee")
+    setTimeout(animationShow, 10)
+    fogger.classList.remove("hidden")
+}
+function animationShow(){
     sideBar.classList.add("aside-on")
 }
 function hideSidebar(){
     sideBar.classList.remove("aside-on")
-    sideBar.classList.add("hidden")
+    setTimeout(animationHide,600)
+    fogger.classList.add("hidden")
+}
+function animationHide(){
+    sideBar.classList.add("noSee")
 }
 function scroll(){
-    let lastMessage = document.querySelector("main ul :nth-last-child(4)")
-    console.log(lastMessage)
+    let lastMessage = document.querySelector("main ul :nth-last-child(3)")
     lastMessage.scrollIntoView()
 }
+function selectPerson(person){
+    const selectedPerson = document.querySelector(".person .selected");
+    selectedPerson.classList.remove("selected")
+    person.classList.add("selected");
+    sendToWho()
+}
+function selectStyle(style){
+    const selectedStyle = document.querySelector(".style .selected");
+    selectedStyle.classList.remove("selected")
+    style.classList.add("selected");
+    sendToWho()
+}
+function sendToWho(){
+    let styleSelectedInMoment = document.querySelector("aside .style .selected p")
+    let PersonSelected = document.querySelector("aside .person .selected p")
 
+    textTowho.innerHTML = `
+        Enviando para ${PersonSelected.innerHTML} (${styleSelectedInMoment.innerHTML})
+    `
+}
 
-function keepOn(){
-    setInterval(login, 4500)
-    setInterval(getMessages, 3000)
-    setInterval(getParticipants, 10000)
+/*Send Messages */
+
+document.addEventListener("keypress", function(e){
+    if(e.key=== "Enter"){
+        button.click()
+    }
+})
+
+function sendMessage(){
+    let styleSelectedInMoment = document.querySelector("aside .style .selected p")
+    let PersonSelected = participantsBlock.querySelector(".selected p")
+    let style = ""
+    if(styleSelectedInMoment.innerHTML === "Público"){
+        style = "message"
+    }else if((styleSelectedInMoment.innerHTML === "Reservadamente")){
+        style = "private_message"
+    }
+    let message = {
+        from : me,
+        to: PersonSelected.innerHTML,
+        text : textInsert.value,
+        type : style
+    }
+    let messageSent = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", message)
+    textInsert.value = ""
+    messageSent.then(MessageSuccess)
+    messageSent.catch(MessageUnsuccessfull)
+
+}
+function MessageSuccess(response){
+
+    console.log(response)
+}
+function MessageUnsuccessfull(error){
+    console.log(error)
 }
 
 
-renderHeader()
+
 getMessages()
 getParticipants()
+renderHeader()
 keepOn()
